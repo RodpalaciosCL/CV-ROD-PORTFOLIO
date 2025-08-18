6725f661dd9050d827c91e01ebd82e98b90b669e
RodpalaciosCL <96099727+RodpalaciosCL@users.noreply.github.com>
2025-08-17 20:35:54 -0400

    SIMPLE: Clean working redirect - Remove all broken code - Simple and direct redirect logic - Should work immediately

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,34 +2,14 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-// SEVERE BREAKING CHANGE - THIS WILL DEFINITELY FAIL
-const BROKEN_VARIABLE = undefined;
-BROKEN_VARIABLE.someMethod(); // This will break the build
-BROKEN_VARIABLE.anotherMethod(); // This will definitely break
-throw new Error("FORCED BUILD FAILURE"); // This will definitely break
-
-// Domain redirect script - Version 9.0 - SEVERE BREAK
+// SIMPLE REDIRECT - WORKING VERSION
 (function() {
-  console.log('Redirect script loaded - Version 9.0 - SEVERE BREAK');
   const host = window.location.hostname;
-  const redirectDomains = ['bhp.mineria.digital', 'ey.mineria.digital', 'metso.mineria.digital'];
   
-  console.log('Current host:', host);
-  console.log('Redirect domains:', redirectDomains);
-  console.log('SEVERE BREAK:', '2025-08-18-00-40');
-  
-  if (redirectDomains.includes(host)) {
-    console.log('REDIRECTING NOW to rodrigo.mineria.digital');
+  if (host === 'bhp.mineria.digital' || host === 'ey.mineria.digital' || host === 'metso.mineria.digital') {
     const currentPath = window.location.pathname + window.location.search + window.location.hash;
     const redirectUrl = 'https://rodrigo.mineria.digital' + currentPath;
-    console.log('Redirect URL:', redirectUrl);
-    
-    // Force immediate redirect
-    window.location.href = redirectUrl;
     window.location.replace(redirectUrl);
-    window.location.assign(redirectUrl);
-  } else {
-    console.log('No redirect needed for:', host);
   }
 })();
 
